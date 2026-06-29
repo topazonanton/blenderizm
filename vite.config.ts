b29d7e20696b64bbf31e0c73a8bf481a749cbd23
Blenderizm <topazonanton@gmail.com>
2026-06-29 13:43:23 +0300

    Update repo name and asset paths to topazonanton/blenderizm

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/remix_-blenderizm/',
+    base: '/blenderizm/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
